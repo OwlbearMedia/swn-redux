9398bc2f74ee1edb550b24b72367396fa3abfc8c
Dylan Whitney <thedylanwhitney@gmail.com>
2017-06-04 14:05:49 -0700

    Updating prod server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
 const serveStatic = require('serve-static');
 
 const app = express();
-app.use(serveStatic(__dirname));
+const dir = path.join(__dirname, 'dist');
+app.use(serveStatic(dir));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started '+ port);
+console.log(`server started on ${port}`);
